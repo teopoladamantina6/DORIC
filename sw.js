2b254b746cc75d916ef8541ef1f2b779e051d578
teopoladamantina6 <teopoladamantina6@gmail.com>
2026-08-08 21:20:37 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+const C='doric-v1';
+const FILES=['./','./index.html','./manifest.json','./icon-180.png','./icon-512.png'];
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(C).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting()));
+});
+self.addEventListener('activate',e=>{
+  e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+});
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET') return;
+  e.respondWith(
+    caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{
+      const cp=res.clone();
+      caches.open(C).then(c=>c.put(e.request,cp)).catch(()=>{});
+      return res;
+    }).catch(()=>caches.match('./index.html')))
+  );
+});
